40a382da1679181d54ed11c556bf01931a7a399d
Jenny-Lim <180jnlm@gmail.com>
2023-12-17 21:26:54 -0500

    ya

--- Source.cpp
+++ Source.cpp
@@ -16,7 +16,7 @@
 
 // Jenny Lim (6978118), Patrick Leonard (stud#)
 
-#define FILENAME "img.tif"
+#define _USE_MATH_DEFINES
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,42 +25,37 @@
 #include <FreeImage.h>
 #include <iostream>
 
-int a[3] = { 10,10,10 }, b[3] = { 10,-10,10 }, c[3] = { -10,-10,10 }, d[3] = { -10,10,10 },
-e[3] = { 10,10,-10 }, f[3] = { 10,-10,-10 }, g[3] = { -10,-10,-10 }, h[3] = { -10,10,-10 };
+float playerSpeed = 1;
+float xpos = 0.0;
+float ypos = 0.0;
+float startingPos = -200;
+float angle = 0.0;
 
-//float angle = 0.0;
-float pos = 0.0;
-
-void drawcube(void) {
+void drawship() {
     glColor3f(1.0, 1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
 
-    glBegin(GL_LINE_LOOP);
-    glVertex3iv(a);
-    glVertex3iv(b);
-    glVertex3iv(c);
-    glVertex3iv(d);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex3iv(a);
-    glVertex3iv(e);
-    glVertex3iv(f);
-    glVertex3iv(b);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex3iv(d);
-    glVertex3iv(h);
-    glVertex3iv(g);
-    glVertex3iv(c);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex3iv(e);
-    glVertex3iv(f);
-    glVertex3iv(g);
-    glVertex3iv(h);
+    glBegin(GL_POLYGON);
+    glVertex2i(240, 240);
+    glVertex2i(240, 260);
+    glVertex2i(260, 260);
+    glVertex2i(260, 240);
+
     glEnd();
     glFlush();
 } // drawcube
+
+void drawasteroid() {
+    glColor3f(1.0, 1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < 6; i++) {
+        glVertex2f(sin(i / 6.0 * 2 * M_PI) * 10, cos(i / 6.0 * 2 * M_PI) * 10);
+    }
+    glEnd();
+    glFlush();
+} // drawasteroid
 
 void draw() {
     Sleep(10);
@@ -68,18 +63,31 @@ void draw() {
 
     glMatrixMode(GL_MODELVIEW);
 
-    if (GetKeyState('A') & 0x8000) { // hold key to move - works now
-        pos = pos - 0.1;
+    if (GetKeyState('A') & 0x8000) { // left
+        xpos = xpos - playerSpeed;
     }
-    if (GetKeyState('D') & 0x8000) {
-        pos = pos + 0.1;
+    if (GetKeyState('D') & 0x8000) { // right
+        xpos = xpos + playerSpeed;
+    }
+    if (GetKeyState('W') & 0x8000) { // up
+        ypos = ypos + playerSpeed;
+    }
+    if (GetKeyState('S') & 0x8000) { // down
+        ypos = ypos - playerSpeed;
     }
 
     glPushMatrix();
-    //glRotatef(angle, 0.0, 1.0, 0.0);
-    glTranslatef(pos, -10, 0);
-    drawcube();
+    glTranslatef(xpos, startingPos + ypos, 0);
+    drawship();
     glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(250, 200, 0); // get random pos + make list of asteroids
+    glRotatef(angle, 0, 0, 1);
+    drawasteroid();
+    glPopMatrix();
+
+    angle++;
     
     glutSwapBuffers();
 } // draw
@@ -122,11 +130,7 @@ int main(int argc, char** argv) {
     glutIdleFunc(draw);
 
     glMatrixMode(GL_PROJECTION);
-    glOrtho(-30.0, 30.0, -30.0, 30.0, -30.0, 30.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glRotatef(30.0, 1.0, 0.0, 0.0);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    gluOrtho2D(0.0, 499.0, 0.0, 499.0);
 
     glutMainLoop();
 } // main
